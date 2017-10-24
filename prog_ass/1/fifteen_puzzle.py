d19382d23c9e3b408e006bb5223c854bbbb997e8
Josiah <hopkinsjosiah@gmail.com>
2017-10-23 17:49:18 -0700

    Completed Fifteen puzzle and basic testing

--- prog_ass/1/fifteen_puzzle.py
+++ prog_ass/1/fifteen_puzzle.py
@@ -1,13 +1,34 @@
-class fifteen_puzzle:
-
+class fifteen_puzzle:	
 	def __init__(self):
-		
-		self.data = [" " * (3 - (len(str(i + 1)))) + str(i + 1) for i in range(15)]
-		self.data.append("000")
+		keys = range(15)
+		values = [" " * (3 - (len(str(i + 1)))) + str(i + 1) for i in range(15)]	
+		self.empty = 15
+		self.data = range(16)
+		self.mapper = dict(zip(keys, values))
+		self.mapper.update({15:" XX"})
 
+	def shift(self, shift_key):
+		self.data[self.empty] = self.data[self.empty + shift_key]
+		self.data[self.empty + shift_key] = 15
+		self.empty = self.empty + shift_key		
+
+	def move(self, op):
+		if op == "up" and self.empty > 3:
+			self.shift(-4)
+		elif op == "down" and self.empty < 12 :
+			self.shift(4)
+		elif op == "left" and self.empty % 4 != 0:
+			self.shift(-1)
+		elif op == "right" and self.empty% 4 != 3:
+			self.shift(1)
+	
+	def is_in_order(self):
+		return self.data == sorted(self.data) 
 
 	def format(self):
 		to_return = ""
 		for i in range(4):
-			to_return += str(self.data[4 * i: 4 * i + 4]) + "\n"
+			for j in self.data[4 * i: 4 * i + 4]:
+				to_return += self.mapper[j]
+			to_return += "\n"
 		return to_return
--- prog_ass/1/tester.py
+++ prog_ass/1/tester.py
@@ -2,7 +2,20 @@ from fifteen_puzzle import *
 
 def main():
 	tester = fifteen_puzzle()
-	print tester.format()
+	print "Is in order: %s" % str(tester.is_in_order())
+	print_puzzle(tester)
+	
+	for i in ["up", "left", "down", "right"]:
+		print str(i)
+		for j in range(6):
+			print j
+			tester.move(i)
+			print_puzzle(tester)
+			print "Is in order: %s" % str(tester.is_in_order())
+
+def print_puzzle(the_puzzle):
+	print "--------------------------------------------------------------"
+	print the_puzzle.format()
 
 if __name__ == '__main__':
 	main()
