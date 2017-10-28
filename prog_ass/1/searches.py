774157dae8af6b9be85289b0da9feece733fbd93
Josiah <hopkinsjosiah@gmail.com>
2017-10-28 11:04:48 -0700

    Added abstractness to search

--- prog_ass/1/searches.py
+++ prog_ass/1/searches.py
@@ -1,3 +1,49 @@
 class search:
 
+	visited = []
+	fringe = []
+
+	def get_visited():
+		raise NotImplementedError
 	
+	def get_fringe():
+
+		raise NotImplementedError
+	
+	def peek():
+		raise NotImplementedError
+
+
+	def pop():
+		raise NotImplementedError
+
+	
+	def get_expanded_number():
+		raise NotImplementedError
+
+
+	def get_max_fringe_size():
+		raise NotImplementedError
+
+
+	def is_heuristic_guided():
+		raise NotImplementedError
+
+
+	def get_depth():
+		raise NotImplementedError
+
+
+	def get_number_of_created_nodes():
+		raise NotImplementedError
+
+
+	def get_distance_to_goal(node):
+		raise NotImplementedError
+
+
+	def get_distance_traveled(node):
+		raise NotImplementedError
+
+
+		
