c701f2ff939afbcc5f969ab2aa56555f83632e76
josiahhopkins <hopkinsjosiah@gmail.com>
2017-12-08 19:45:04 -0800

    Just built alpha beta and minimax

--- prog_ass/2/game_player.py
+++ prog_ass/2/game_player.py
@@ -1,27 +1,48 @@
-class game_player:
-	
-	def get_move(self):
-		raise NotImplementedError
+def alpha_beta(depth, min, node, alpha, beta)
+	if node.game_state.win_state != ' ' or depth == 0:
+		return node.game_state.heuristic()
+	value = 100000 
+	chosen_move = None
+	if not min:
+		value = -100000
+	for child in node.get_children():
+		child_value = alpha_beta(depth -1 , not min, child, alpha, beta)
+		if child_value != None
+			if min:
+				if child_value > value:
+					chosen_move = child.last_move
+				value = max(value, child_value)
 
-class human:
-	def get_move():
-		raise NotImplementedError
+				alpha = max(value, alpha)
+			else: 
+				if child_value < value:
+					chosen_move = child.last_move
+				value = min(value, child_value)
+				beta = min(value, beta)
+			if beta <= alpha:
+				return (value, chosen_move)
+	return (value, chosen_move)
 
-class cli_human:
-	def get_choice():
-		
-class alpha_beta:
-	def get_move(depth, min, node, alpha, beta)
-
-		return (value, chosen_move)
-
-class minimax:
-
-
+def minimax(depth, min, node):
+	if node.game_state.win_state != ' ' or depth == 0:
+		return node.game_state.heuristic()
+	value = 100000 
+	chosen_move = None
+	if not min:
+		value = -100000
+	for child in node.get_children():
+		child_value = alpha_beta(depth -1 , not min, child)
+		if min:
+			if child_value > value:
+				chosen_move = child.last_move
+			value = max(value, child_value)
+		else: 
+			if child_value < value:
+				chosen_move = child.last_move
+			value = min(value, child_value)
+	return (value, chosen_move)
 
 class game:
-	
-
 	def __init__(self, p1=False, p2=True, minimax=False):
 		self.current_game = pentago_node(pentago())
 		if p1:
